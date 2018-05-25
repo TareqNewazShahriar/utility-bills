dbe6ee42f4485dca786e63beb200d5ba20ccbc59
TareqNewazShahriar <tareq.newaz.shahriar@gmail.com>
2018-05-26 05:03:11 +0600

    BillDetailsActivity.java: import added

--- Src/app/src/main/java/com/doesnothaveadomain/splitunitcost/BillDetailsActivity.java
+++ Src/app/src/main/java/com/doesnothaveadomain/splitunitcost/BillDetailsActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
